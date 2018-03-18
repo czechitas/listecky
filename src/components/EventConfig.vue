3f4fc342db3d262491a63c21c471dea885d72fab
Jan Vlnas <git@jan.vlnas.cz>
2018-03-18 20:36:55 +0100

    EventConfig WIP

--- src/components/EventConfig.vue
+++ src/components/EventConfig.vue
@@ -1,16 +1,22 @@
 <template>
 <div>
-<h2>Event Config {{ eventId }}</h2>
-<form v-on:submit.prevent="onSubmit">
-  <b-field label="Event Identifier">
-    <b-input v-model="eventId" />
-  </b-field>
-  <p class="control">
-    <button type="submit" class="button is-primary">
-      Create Event
-    </button>
-  </p>
-</form>
+  <h1 class="title is-1">Event Configuration</h1>
+  <form v-on:submit.prevent="onSubmit">
+    <b-field label="Room Layout">
+      <b-input type="textarea" v-model="layout"></b-input>
+    </b-field>
+    <b-field label="Tasks">
+      <b-input type="textarea" v-model="tasks"></b-input>
+    </b-field>
+    <p class="control">
+      <button type="submit" class="button is-primary">
+        Update Event
+      </button>
+    </p>
+  </form>
+
+  <h2 class="title is-3">Students</h2>
+  <pre><code>{{students}}</code></pre>
 </div>
 </template>
 
@@ -22,15 +28,42 @@ export default {
   props: ['eventId', 'gun'],
   data () {
     return {
+      layout: '',
+      tasks: '',
+      students: '',
     }
   },
   mounted () {
-    console.log(this.gun)
+    const paths = ['layout', 'tasks', 'students']
+    for (const path of paths) {
+      this.gun.get(path).once(value => {
+        if (typeof value === 'object') {
+          value = JSON.stringify(value, undefined, 2)
+        }
+        this[path] = value
+      })
+    }
+    /*gun.get('layout').on(layout => {
+      this.layout = layout
+    })
+
+    gun.get('tasks').on(tasks => {
+      this.tasks = tasks
+    })
+
+    gun.get('').on(tasks => {
+      this.tasks = tasks
+    })*/
   },
   methods: {
     onSubmit () {
-      console.log(this.gun)
-      const {eventId} = this
+      try {
+        this.gun.get('layout').put(this.layout)
+      } catch (e) {
+        console.error(e)
+      }
+
+      this.gun.get('tasks').put(this.tasks)
     }
   }
 }
