39f29391dbe4cc5daebda184801f3b2438e9a3e3
Jan Vlnas <git@jan.vlnas.cz>
2018-03-18 16:30:34 +0100

    Add buefy

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Buefy from 'buefy'
 import App from './App'
 import router from './router'
+import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Buefy)
 
 /* eslint-disable no-new */
 new Vue({
