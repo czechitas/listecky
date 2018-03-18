0720198426397fecf42cb45a732c71c1672e4990
DanielSrb <srb.dan@gmail.com>
2018-03-18 17:41:49 +0100

    WIP: RoomMap component fixtures moved to components added more complex desk to fixture

---
+++ src/components/RoomMap.vue
@@ -0,0 +1,30 @@
+<script>
+import DefaultLayout from '../fixtures/layout'
+
+export default {
+  name: 'RoomMap',
+  data () {
+    return {
+      'map': DefaultLayout
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <div
+      v-for="(desk, deskIndex) in map.desks"
+      v-bind:desk="desk"
+      v-bind:index="deskIndex"
+      v-bind:key="deskIndex"
+    >
+
+      <pre>{{ desk }}</pre>
+      <br>{{ desk.name }}
+
+      <br>{{ desk.layout }}
+      <br>
+    </div>
+  </div>
+</template>
