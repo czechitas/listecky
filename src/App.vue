2e499684289572d07d265c96e1946b70edd70004
Honza Pobořil <honza@poboril.cz>
2018-03-18 21:08:10 +0100

    Added i18n.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
-<template>
-  <div id="app">
-    <router-view/>
-  </div>
-</template>
-
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      locale: 'en',
+      languages: ['en', 'cz']
+    }
+  },
+  watch: {
+    locale (val) {
+      this.$i18n.locale = val
+    }
+  }
 }
 </script>
 
-<style>
-</style>
+<template>
+  <div id="app">
+    <router-view/>
+    <select v-model="locale" style="position: fixed; right: 0; bottom: 0">
+      <option v-for="code in languages">{{ code }}</option>
+    </select>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,25 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
 import Buefy from 'buefy'
+import 'buefy/lib/buefy.css'
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+
 import App from './App'
 import router from './router'
-import 'buefy/lib/buefy.css'
 
 Vue.config.productionTip = false
 
 Vue.use(Buefy)
 
+Vue.use(VueI18n)
+
+const i18n = new VueI18n({
+  locale: 'en'
+})
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  i18n,
   router,
   components: { App },
   template: '<App/>'
